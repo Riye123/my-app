be70e9137a681af50518ba5f6f8cddc23284ef1f
my-app <harrison36058602@gmail.com>
2024-05-17 16:38:09 +0800

    選單變色,底線

--- app/page.js
+++ app/page.js
@@ -1,38 +1,34 @@
 import Link from "next/link";
-import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle,Footer, FooterCopyright, FooterLink, FooterLinkGroup,Carousel} from "flowbite-react";
+import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle,Footer, FooterCopyright, FooterLink, FooterLinkGroup,Carousel,Card,DarkThemeToggle} from "flowbite-react";
 
 export default function Component() {
   return (
     <>
-        <Navbar fluid rounded>
-      <NavbarBrand href="https://flowbite-react.com">
-        <img src="/next.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-      </NavbarBrand>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
-        <NavbarToggle />
+    <div className="bg-[#cccccc]">
+      <div className="container mx-auto">
+          <Navbar fluid rounded className="bg-blue-400">
+        <NavbarBrand as={Link} href="/">
+          <img src="https://www.yuntech.edu.tw/images/website_png/Group_640.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+          <span className="self-center whitespace-nowrap text-xl font-semibold text-white">Hao Bro website</span>
+        </NavbarBrand> 
+        <div className="flex md:order-2">
+          <Button>Get started</Button>
+          <NavbarToggle />
+        </div>
+        <NavbarCollapse>
+          <NavbarLink href="#" className="text-dark">
+            <span className="px-4 py-2 hover:text-yellow-500 hover:border-yellow-500 hover:border-b-2">活動</span>
+          </NavbarLink>
+          <NavbarLink as={Link} href="#" className="text-dark hovor:text-yellow-50 hover:hover-b">交通</NavbarLink>
+          <NavbarLink href="#">關於我們</NavbarLink>
+          <NavbarLink href="#">Pricing</NavbarLink>
+          <NavbarLink href="#">Contact</NavbarLink>
+        </NavbarCollapse>
+        <DarkThemeToggle />
+      </Navbar>
       </div>
-      <NavbarCollapse>
-        <NavbarLink href="#" active>
-          Home
-        </NavbarLink>
-        <NavbarLink href="#">About</NavbarLink>
-        <NavbarLink href="#">Services</NavbarLink>
-        <NavbarLink href="#">Pricing</NavbarLink>
-        <NavbarLink href="#">Contact</NavbarLink>
-      </NavbarCollapse>
-    </Navbar>
-        <Footer container>
-        <FooterCopyright href="#" by="Flowbite™" year={2022} />
-        <FooterLinkGroup>
-          <FooterLink href="#">About</FooterLink>
-          <FooterLink href="#">Privacy Policy</FooterLink>
-          <FooterLink href="#">Licensing</FooterLink>
-          <FooterLink href="#">Contact</FooterLink>
-        </FooterLinkGroup>
-      </Footer>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+    </div>
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
         <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
@@ -41,6 +37,34 @@ export default function Component() {
         <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
       </Carousel>
     </div>
+        <Card className="max-w-sm">
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+          </p>
+          <Button>
+            Read more
+            <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Button>
+        </Card>
+        <Footer container >
+        <FooterCopyright href="#" by="Flowbite™" year={2022} />
+        <FooterLinkGroup>
+          <FooterLink href="#">About</FooterLink>
+          <FooterLink href="#">Privacy Policy</FooterLink>
+          <FooterLink href="#">Licensing</FooterLink>
+          <FooterLink href="#">Contact</FooterLink>
+        </FooterLinkGroup>
+      </Footer>
+
     </>
     );
 }
