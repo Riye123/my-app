0c2d53ea70e5afceed1adbb36b70cd5140e1b4ab
my-app <harrison36058602@gmail.com>
2024-05-24 16:57:02 +0800

    網頁下的卡片

--- app/page.js
+++ app/page.js
@@ -1,7 +1,28 @@
+/** 
+ * @author 邵聖皓 <harrison0975294673@gmail.com>
+*/
 import Link from "next/link";
 import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle,Footer, FooterCopyright, FooterLink, FooterLinkGroup,Carousel,Card,DarkThemeToggle} from "flowbite-react";
 
 export default function Component() {
+
+  const items = [
+    {
+      cover: "/Tower_of_Taipei_101(cropped).jpg",
+      name: "台北101",
+      discription: "台北101(英文:Taipei 101)",
+    },
+    {
+      cover: "/31257721794_a7f2e036ab_o.jpg",
+      name: "火車站",
+      discription: "",
+    },
+    {
+      cover: "/Yuntech_20100910_Cloud_Octagon_0019.jpg",
+      name: "雲科大校門",
+      discription: "",
+    }
+  ]
   return (
     <>
     <div className="bg-[#cccccc]">
@@ -30,31 +51,38 @@ export default function Component() {
     </div>
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="/Tower_of_Taipei_101(cropped).jpg" alt="由 AnthonySantiago101 - 自己的作品, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=113982887" />
+        <img src="/31257721794_a7f2e036ab_o.jpg" alt="Copyright by https://www.flickr.com/photos/405mi16/" />
+        <img src="/Yuntech_20100910_Cloud_Octagon_0019.jpg" alt="由 Zhuang kuonan - 自己的作品, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=65669091" />
         <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
       </Carousel>
     </div>
-        <Card className="max-w-sm">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-          </p>
-          <Button>
-            Read more
-            <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </Button>
-        </Card>
+    <div className="container mx-auto grid grid-cols-4 gap-4">
+        { items.map( items =>
+          <Card className="max-w-sm"
+              imgAlt={items.name}
+              imgSrc={items.cover}>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {items.name}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {items.discription}
+            </p>
+            <Button>
+              Read more
+              <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Button>
+          </Card>
+          )
+        }
+        </div>
         <Footer container >
         <FooterCopyright href="#" by="Flowbite™" year={2022} />
         <FooterLinkGroup>
