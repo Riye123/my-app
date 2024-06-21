105561bbb5c249d565cf5cee401ec700480a4e84
my-app <harrison36058602@gmail.com>
2024-06-21 16:41:20 +0800

    card期末設計

--- Components/Card.jsx
+++ Components/Card.jsx
@@ -1,10 +1,14 @@
-import Image from "next/image"
+import { Button } from "flowbite-react";
 
 export default function CustomCard({item}){
     return (
-        <div className="border">
-            <Image src={item.Picture.PictureUrl1} alt={item.Picture.PictureDescription1} width={500} height={500}/>
-            <div>{item.ScenicSpotName}</div>
-        </div>
-    )
+            <div className="container mx-auto border-4 border-gray-400 rounded-xl shadow-custom-dark bg-white" style={{margin:'10px'}}>
+                <img  className="rounded-t-lg" src={item.Picture.PictureUrl1} alt={item.DescriptionDetail} height={500} width={500}/>
+                <h3 className="text-2xl font-bold mb-4">{item.ScenicSpotName}</h3>
+                <div style={{margin:'10px'}}>{item.DescriptionDetail}</div>
+                <div className="flex flex-wrap gap-2 ">
+                    <Button gradientDuoTone="pinkToOrange">Blue</Button>
+                </div>
+            </div>
+    )   
 }
--- app/page.js
+++ app/page.js
@@ -61,33 +61,8 @@ export default function Component() {
       </Carousel>
     </div>
 
-    <div className="container mx-auto grid grid-cols-4 gap-4">
-        { items.map( items =>
-          <Card className="max-w-sm"
-              imgAlt={items.ScenicSpotName}
-              imgSrc={items.Picture.PictureUrl1}>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {items.ScenicSpotName}
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              {items.DescriptionDetail}
-            </p>
-            <Button>
-              Read more
-              <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Button>
-          </Card>
-          )
-        }
-        </div>
         <div className="container mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4">
             { items.map( (items,index) =>
               <CustomCard item={items} key={index}/>
             )}
